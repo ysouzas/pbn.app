89a8916e1ed9a36fdb6de64e9604697a5afd7f2f
Santos <yagosouza.santos@finastra.com>
2023-04-13 14:44:43 +0100

    refactoring

---
+++ src/PBN.APP/Constants/ConnectionStringConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace PBN.APP.Constants
+{
+    public static class ConnectionStringConstants
+    {
+        public static string BaseUrl { get; } = string.Empty;
+
+        public static string TeamUrl { get; } = $"{BaseUrl}/teams";
+        public static string PlayerUrl { get; } = $"{BaseUrl}/football_functions";
+
+    }
+}
